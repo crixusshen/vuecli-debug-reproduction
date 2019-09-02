dec02739870070e924623bcb4a9bb310d3cb5eb8
dfzq <dfzq@MacBookPro.local>
2019-09-02 11:05:26 +0800

    feat(all): init a test project
    #type: 用来描述commit的类别，只能是下面7个标识
    #     feat:新功能（feature）
    #     fix:修复bug
    #     doc:文档改变
    #     style:代码格式改变（不影响代码运行的变动，如样式、代码美化）
    #     refactor:某个已有功能重构（即不是新增功能，也不是修改bug的代码变动）
    #     test:增加测试
    #     chore:改变了build工具,如grunt换成了npm（构建过程或辅助工具的变动）
    #
    #scope: 用来说明此次修改的影响范围
    #     all:表示影响面大,如修改了网络框架会对整个程序产生影响
    #     location:表示影响小,某个小小的功能
    #     module:表示会影响某个模块,如登录模块、首页模块、用户管理模块等等
    #
    #subject: 用来简要描述本次改动的概述

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<!--
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @Date: 2019-09-01 10:43:42
+ * @LastEditTime: 2019-09-02 11:02:47
+ * @LastEditors: Please set LastEditors
+ -->
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld2 msg="Welcome to Your React.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/A/HelloWorld.vue'
+import HelloWorld2 from './components/B/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    HelloWorld2
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
